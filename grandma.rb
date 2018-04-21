693b2907194edce769ab36a44798510bbf718346
George  Pianka <gpianka1@gmail.com>
2018-04-21 06:48:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  phrase.upcase?
+  !phrase == phrase.upcase
+  return "Huh"
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
